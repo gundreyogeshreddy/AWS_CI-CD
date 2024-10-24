6b4637d2ede34f6f6202fdfa20d32f95401851a7
Yogesh <136956748+gundreyogeshreddy@users.noreply.github.com>
2024-10-24 12:46:38 +0530

    Delete simple-python-app/scripts directory

--- simple-python-app/scripts/start_container.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-# Pull the Docker image from Docker Hub
-docker pull yogesh2024/simple-python-flask-app:latest
-
-# Run the Docker image as a container
-docker run -d -p 5000:5000 yogesh2024/simple-python-flask-app:latest
